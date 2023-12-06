7a28d32a174de14968cca198a1f945e11ee30982
BrunoItacaramby <brunobob69@hotmail.com>
2023-12-05 21:27:46 -0300

    Collector funcionando 100%

---
+++ YoutubeDownloader.js
@@ -0,0 +1,14 @@
+import verificarAreaDeTransferencia from "./core/links.js";
+
+class YoutubeDownloader {
+
+    static Collector() {
+        // Verificar a área de transferência a cada segundo
+        setInterval(verificarAreaDeTransferencia, 1000);
+        console.log('Aguardando a detecção de novos links do YouTube...');
+    }
+}
+
+export default YoutubeDownloader
+
+YoutubeDownloader.Collector()
--- core/links.js
+++ core/links.js
@@ -38,7 +38,4 @@ function verificarAreaDeTransferencia() {
   }
 }
 
-// Verificar a área de transferência a cada segundo
-setInterval(verificarAreaDeTransferencia, 1000);
-
-console.log('Aguardando a detecção de novos links do YouTube...');
+export default verificarAreaDeTransferencia
